997eca468a5311bd91e6b78c631e97cabe371b3f
suicide <111202920+x64suicide@users.noreply.github.com>
2022-10-17 21:09:18 +0200

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,17 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
+end)
+
+RegisterCommand('seat', function(src, args, raw)
+    if IsPedInAnyVehicle(PlayerPedId()) then
+        SetPedIntoVehicle(PlayerPedId(), GetVehiclePedIsIn(PlayerPedId()), args[1])
+        ESX.ShowNotification('~g~Zmieniono siedzenie')
+    else
+        ESX.ShowNotification('~r~Nie jesteś w pojeździe')
+    end
+end)
